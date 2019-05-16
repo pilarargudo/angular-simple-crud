adbe180d324888ddaf2dab3c581e67a3ffaac2c6
Pilar Argudo <pargudo@disid.com>
2019-05-16 22:29:21 +0200

    Added employee model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  title = 'angular simple crud';
+
+  // employeeArray: Employee[];
+  employeeArray: Employee[] = [
+    {id: 1, name: 'María', country: 'Spain'},
+    {id: 2, name: 'Antonio', country: 'Colombia'},
+    {id: 3, name: 'Jane', country: 'USA'}
+  ];
+
+  // ngModel directive
+  // variable propiedad de la clase de tipo Employee
+  selectedEmployee: Employee = new Employee();
+  
+  // method
+
 }
